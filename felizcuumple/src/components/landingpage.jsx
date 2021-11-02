6d263e79f891e977b923c7d49c15200ea71bf992
fernandoschulzz <64163131+fernandoschulzz@users.noreply.github.com>
2021-11-02 20:56:02 -0300

    d

--- felizcuumple/src/components/landingpage.jsx
+++ felizcuumple/src/components/landingpage.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import pic1 from "../images/blue-balloon.png"
+import style from "./landingpage.module.css"
 
 export default function landingpage() {
     return (
@@ -6,6 +8,17 @@ export default function landingpage() {
             <h1>Bienvenido a </h1>
             <h1>la pagina de </h1>
             <h1>REGALO Del SANTY</h1>
+            <button type="button" class="btn btn-danger">Recibir Regalo</button>
+            <div className={style.set}>
+                <div className={style.bone1}><img src={pic1} alt="" /></div>
+                <div className={style.bone2}><img src={pic1} alt="" /></div>
+                <div className={style.bone3}><img src={pic1} alt="" /></div>
+                <div className={style.bone4}><img src={pic1} alt="" /></div>
+                <div className={style.bone5}><img src={pic1} alt="" /></div>
+                <div className={style.bone6}><img src={pic1} alt="" /></div>
+                <div className={style.bone7}><img src={pic1} alt="" /></div>
+                <div className={style.bone8}><img src={pic1} alt="" /></div>
+            </div>
         </div>
     )
 }
