e789715212b834f0a28451a23fb232b90b62d90c
fernandoschulzz <64163131+fernandoschulzz@users.noreply.github.com>
2021-11-02 20:18:04 -0300

    hola

--- felizcuumple/src/components/landingpage.jsx
+++ felizcuumple/src/components/landingpage.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function landingpage() {
     return (
         <div>
-            <h1>hello santy de mi vida</h1>
+            <h1>Bienvenido a </h1>
+            <h1>la pagina de </h1>
+            <h1>REGALO Del SANTY</h1>
         </div>
     )
 }
