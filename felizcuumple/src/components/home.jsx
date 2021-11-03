bf980de66eaf67a7270b0b62b89866767a6fd94e
fernandoschulzz <64163131+fernandoschulzz@users.noreply.github.com>
2021-11-02 21:26:47 -0300

    add

--- felizcuumple/src/components/home.jsx
+++ felizcuumple/src/components/home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <div className={style.back}>
             <h3>Antes quiero decirte Algo</h3>
-            <h5>Que te amo muchisisimo y que me haces muy feliz siempre</h5>
+            <h5>Que te amo muchisisimooo y que me haces muy feliz siempre</h5>
             <h5>REGALO Del SANTY</h5>
             <Link to="/home">
             <button type="button" className="btn btn-danger">Recibir Regalo</button>
