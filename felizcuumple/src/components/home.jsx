dc9a9f3b78955256704a23baa2d6a2a62ce1fe91
fernandoschulzz <64163131+fernandoschulzz@users.noreply.github.com>
2021-11-02 21:38:47 -0300

    hola

--- felizcuumple/src/components/home.jsx
+++ felizcuumple/src/components/home.jsx
@@ -1,18 +1,16 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-// import pic1 from "../images/blue-balloon.png"
-// import style from "./home.module.css"
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-// export default function Home() {
-//     return (
-//         <div className={style.back}>
-//             <h3>Antes quiero decirte Algo</h3>
-//             <h5>Que te amo muchisisimooo y que me haces muy feliz siempre</h5>
-//             <h5>REGALO Del SANTY</h5>
-//             <Link to="/home">
-//             <button type="button" className="btn btn-danger">Recibir Regalo</button>
-//             </Link>
-//         </div>
-//     )
-// }
+export default function Home() {
+    return (
+        <div>
+            <h3>Antes quiero decirte Algo</h3>
+            <h5>Que te amo muchisisimooo y que me haces muy feliz siempre</h5>
+            <h5>REGALO Del SANTY</h5>
+            <Link to="/home">
+            <button type="button" className="btn btn-danger">Recibir Regalo</button>
+            </Link>
+        </div>
+    )
+}
 
