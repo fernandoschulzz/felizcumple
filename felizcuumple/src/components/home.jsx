92d1c8252603da2576501e111415c047a433e0bd
fernandoschulzz <64163131+fernandoschulzz@users.noreply.github.com>
2021-11-02 21:13:54 -0300

    d

--- felizcuumple/src/components/home.jsx
+++ felizcuumple/src/components/home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
             <h5>Que te amo muchisisimo y que me haces muy feliz siempre</h5>
             <h5>REGALO Del SANTY</h5>
             <Link to="/home">
-            <button type="button" class="btn btn-danger">Recibir Regalo</button>
+            <button type="button" className="btn btn-danger">Recibir Regalo</button>
             </Link>
         </div>
     )
