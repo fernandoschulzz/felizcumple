cd798b7f529623279af7a823d4abe7db8ec06801
fernandoschulzz <64163131+fernandoschulzz@users.noreply.github.com>
2021-11-02 21:46:13 -0300

    aqq

--- felizcuumple/src/components/home.jsx
+++ felizcuumple/src/components/home.jsx
@@ -5,10 +5,10 @@ export default function Home() {
     return (
         <div>
             <h3>Antes quiero decirte Algo</h3>
-            <h5>Que te amo muchisisimooo y que me haces muy feliz siempre</h5>
-            <h5>REGALO Del SANTY</h5>
+            <h5>primero perdon por atrasar el reagalo.🤪 jajaja Pero antes Tarde que nunca Y Finalmente lo mas importante que te quiero decir es Que te amo muchisisimooo de Verdad y que me haces muy feliz siempre😍😍😍</h5>
+            <h5>Soos lo mejor del Universo🌟</h5>
             <Link to="/home">
-            <button type="button" className="btn btn-danger">Recibir Regalo</button>
+            <button type="button" className="btn btn-danger">Ahora si Recibir Regalo 🎁</button>
             </Link>
         </div>
     )
